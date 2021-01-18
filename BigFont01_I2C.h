bd5c39c267a57de7c03e209453f12f206fea75b2
telleropnul <telleropnul@users.noreply.github.com>
2021-01-18 17:36:13 +1000

    Added option to print large numbers
    Added option to print large numbers

--- BigFont01_I2C.h
+++ BigFont01_I2C.h
@@ -8,7 +8,9 @@ class BigFont01 {
 	public:
 		BigFont01(LiquidCrystal_I2C*);
 		void begin();
-		int write(int, int, char);
+		int  writechar(int, int, char);                // col, row, single ascii character
+		void clear(int, int);                          // col, row
+		void writeint (int, int, int, uint8_t, bool);  // col, row, number to display, digits, leading 
 	private:
 		LiquidCrystal_I2C* _lcd;
 };
---
+++ examples/numbers.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <BigFont01.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); //address, columns, rows
+BigFont01         big(&lcd); // construct large font object, passing to it the name of our lcd object
+
+void setup()
+{
+  lcd.begin();
+  big.begin();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.write ("   BigFont01    ");
+  lcd.setCursor(0,1);
+  lcd.write ("   =========    ");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop()
+{
+  big.writeint(0,3,millis(),4,true); // display milliseconds value using 4 digits with leading zeros enabled starting at row 0 column 5
+}
